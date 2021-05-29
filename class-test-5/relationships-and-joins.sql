5e7b90e78b48fda82a40a172edde6e14ac1cf048
Tobi K <32499735+tkud04@users.noreply.github.com>
2021-05-29 20:33:09 +0100

    Update relationships-and-joins.sql

--- class-test-5/relationships-and-joins.sql
+++ class-test-5/relationships-and-joins.sql
@@ -19,3 +19,27 @@ INNER JOIN film_genre ON films.id=film_genre.film_id
 INNER JOIN genres ON film_genre.genre_id=genres.id
 WHERE genres.id=2 OR genres.id=5;
 
+SELECT certificates.name,count(films.title) as "Number of films" from films 
+INNER JOIN certificates ON films.certificate_id=certificates.id 
+GROUP BY name;
+
+SELECT genres.name,count(films.title) as "Number of films" from films 
+INNER JOIN film_genre ON films.id=film_genre.film_id
+INNER JOIN genres ON film_genre.genre_id=genres.id
+GROUP BY name;
+
+SELECT films.title as "Movie",genres.name  as "Genre",certificates.name  as "Certificate" from films 
+INNER JOIN film_genre ON films.id=film_genre.film_id
+INNER JOIN genres ON film_genre.genre_id=genres.id
+INNER JOIN certificates ON films.certificate_id=certificates.id 
+WHERE films.id=6;
+
+SELECT title as "Movie" from films 
+INNER JOIN film_genre ON films.id=film_genre.film_id
+INNER JOIN genres ON film_genre.genre_id=genres.id
+WHERE genres.id=3;
+
+SELECT certificates.name,avg(films.duration) as "Average duration" from films 
+INNER JOIN certificates ON films.certificate_id=certificates.id 
+GROUP BY name 
+ORDER BY duration DESC;
